1365150470bf86d2ae716373f5eca092a0fe47ff
maclean <maclean@ucar.edu>
2009-01-08 20:07:14 +0000

    more build script fixes

--- trunk/build_dpkg.sh
+++ trunk/build_dpkg.sh
@@ -15,23 +15,29 @@ dpkg=libxmlrpc++
 version=0.7
 fversion=`echo $version | sed 's/\./_/g'`
 
-set -x
+# set -x
 shopt -s nullglob
 
 tmpdir=/tmp/${0##*/}_$$
 trap "{ rm -rf $tmpdir; exit 0; }" EXIT
 
 # Create debian packages from RPMs
-drepo=${rroot%/*}/ael-dpkgs
-[ -d $rroot ] && { [ -d $drepo ] || mkdir -p $drepo; }
+# Where to put the debian packages.
+drepos=(${rroot%/*}/ael-dpkgs /opt/local/ael-dpkgs)
+[ -d $rroot ] && { [ -d $drepos[0] ] || mkdir -p $drepos[0]; }
+[ -d $drepos[1] ] || mkdir -p $drepos[1]
+
 pdir=$tmpdir/$dpkg
 [ -d $pdir ] || mkdir -p $pdir
 
 for arch in arm armbe; do
 
-    rpm=$topdir/RPMS/i386/${pkg}-${arch}-linux-${version}*.i386.rpm
-    [ -z $rpm ] && rpm=$rroot/ael/i386/${pkg}-${arch}-linux-${version}*.i386.rpm
-    [ -z $rpm ] && continue
+    rpm=($topdir/RPMS/i386/${pkg}-${arch}-linux-${version}*.i386.rpm)
+    [ ${#rpm[*]} -eq 0 ] && \
+        rpm=($rroot/ael/i386/${pkg}-${arch}-linux-${version}*.i386.rpm)
+    [ ${#rpm[*]} -eq 0 ] && continue
+    # get last rpm name in case there are multiple versions
+    rpm=${rpm[${#rpm[*]}-1]}            # love that syntax!
 
     rpm2cpio $rpm | ( cd $pdir; cpio -idv )
     mkdir -p $pdir/usr/lib
@@ -44,14 +50,26 @@ for arch in arm armbe; do
 
     fakeroot dpkg -b $pdir
     dpkg-name $tmpdir/${dpkg}.deb
-    [ -d $drepo ] && mv $tmpdir/${dpkg}_*_${arch}.deb $drepo
+    deb=($tmpdir/${dpkg}_*_${arch}.deb)
+    if [ ${#deb[*]} -eq 0 ]; then
+        echo "error, can't find the debian package"
+        exit 1
+    fi
+    for drepo in ${drepos[*]}; do
+        if [ -d $drepo ]; then
+            dfile=${deb[0]}
+            rsync $dfile $drepo || continue
+            dfile=$drepo/${dfile##*/}
+            verfile=${dfile%.deb}.ver
+            cksum $dfile > $verfile
+            dv=`awk '/^Version:/{print $2}' DEBIAN/control`
+            sv=`svnversion .`
+            echo "$dv $sv `date +%Y%m%d%H%M%S`" >> $verfile
+            echo "Debian package: $dfile"
+            echo "Version file: $verfile"
+            cat $verfile
+        fi
+    done
     rm -rf $pdir/*
 done
-
-if [ -d $rroot ]; then
-    arch=i386
-    rpms=($topdir/RPMS/$arch/${pkg}-*-${version}*.$arch.rpm \
-        $topdir/SRPMS/${pkg}-${version}*.src.rpm)
-    copy_ael_rpms_to_eol_repo ${rpms[*]}
-fi
 
